9e5101f169e3f89e975732166b99045e968bb3cf
Aleksej Lebedev <root@zta.lk>
2023-03-30 21:44:20 +0200

    Added remove_const and remove_const_t.

--- include/vegan/types.h
+++ include/vegan/types.h
@@ -15,6 +15,10 @@ namespace vegan {
   template<typename T> struct remove_reference<T &&> { typedef T type; };
   template<typename T> using remove_reference_t = typename remove_reference<T>::type;
 
+  template<typename T> struct remove_const          { typedef T type; };
+  template<typename T> struct remove_const<const T> { typedef T type; };
+  template<typename T> using remove_const_t = typename remove_const<T>::type;
+
   template<typename T> remove_reference_t<T> &&move(T &&x) { return static_cast<remove_reference_t<T> &&>(x); }
 
   template<typename T> class vector_ref;
