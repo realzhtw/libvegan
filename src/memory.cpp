d7125b2164091f3616a6faafe6b4f83301970a9d
Aleksej Lebedev <root@zta.lk>
2023-11-05 13:27:27 +0100

    Using posix_memalign instead of aligned_alloc.

--- src/memory.cpp
+++ src/memory.cpp
@@ -6,7 +6,11 @@ namespace vegan {
 
   void *alloc(Long n, Long alignment)
   {
-    return ::aligned_alloc(alignment, n);
+    if (alignment < 2 * sizeof(void *))
+      return malloc(n);
+    void *p = nullptr;
+    ::posix_memalign(&p, alignment, n);
+    return p;
   }
 
   void free(void *p) { ::free(p); }
